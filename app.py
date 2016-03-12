7b4ed75be574dac18ff24eaa6d2bdbd6224ef489
Christophe Pouliot <cristophepoug@gmail.com>
2016-03-12 13:41:02 -0800

    added js

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 # import the Flask class from the flask module
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # create the application object
 app = Flask(__name__)
+
 
 # use decorators to link the function to a url
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/talk')
+def search():
+    search_term = request.args.get('search')
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
